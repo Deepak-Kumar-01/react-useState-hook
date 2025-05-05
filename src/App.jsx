27cfce0f5984ccbba3fed55549c2422fdb7f773f
Deepak Kumar <deepakkumar224336@gmail.com>
2025-05-05 13:29:42 +0530

    todo app created

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
 import { useState } from "react";
 import "./App.css";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+function TodoCard(props) {
   return (
     <div>
-      <p>count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h4>{props.title}</h4>
+      <h5>{props.description}</h5>
+      <p>{props.isDone ? "Task is done" : "Task is not done"}</p>
     </div>
   );
-};
+}
 function App() {
+  const [todos, setTodos] = useState([]);
+  //fn to add todos
+  function addTodo() {
+    let newArray = [];
+    for (let i = 0; i < todos.length; i++) {
+      newArray.push(todos[i]);
+    }
+    newArray.push({
+      title: document.getElementById("title").value,
+      description: document.getElementById("description").value,
+      isDone: true,
+    });
+    setTodos(newArray);
+  }
   return (
     <div>
-      <Counter />
+      <input id="title" type="text" placeholder="title"></input>
+      <input id="description" type="text" placeholder="description"></input>
+      <br />
+      <button onClick={addTodo}>Add</button>
+      {todos.map((todo) => {
+        return (
+          <TodoCard
+            title={todo.title}
+            description={todo.description}
+            isDone={todo.isDone}
+          />
+        );
+      })}
     </div>
   );
 }
